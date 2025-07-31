2d8d79a6a64a418e466b4068b120fff24f317b30
Dylan Wickwire <dlwicky@hotmail.com>
2025-07-30 20:07:30 -0500

    Step 3 completed - Read and Update Exercises

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const pet = {
     "name": "Buddy",
     "species": "Dog",
-    "age": 5
+    "age": 6
 };
 
 console.log(pet.name);
@@ -12,3 +12,41 @@ pet.age = 6;
 console.log(pet.age);
 
 // TODO: What do you think each console.log will print?
+
+const book = {
+    "title": "JavaScript for Beginners",
+    "author": "Jane Doe",
+    "pages": 250,
+    "isPublished": true
+};
+
+// TODO: List all the keys as a comment
+// Keys: title, author, pages, isPublished
+// TODO: List all the values as a comment
+// Values: "javascript for beginners, jane doe, 250,true
+
+const car = {
+    "make": "Toyota",
+    "model": "Corolla",
+    "year": 2020,
+    "color": "Blue"
+};
+
+// TODO: Fix the syntax error.
+// TODO: Explain what was wrong in a comment.
+console.log(car);
+const sprite = {
+    "name": "Hero",
+    "x": 120,
+    "y": 220
+};
+
+console.log(sprite.x);
+sprite.x = 150;
+console.log(sprite.x);
+
+sprite.y = 250;
+console.log(sprite.y);
+
+// TODO: Write your guesses for each console.log as comments.
+// its going to make a little sprite fella
